9dab4c1db68372938b4a1789cd64f05563c52c22
Arsalan Sadri <asadri62@gmail.com>
2018-12-29 15:38:42 -0800

    minor changes

--- src/index.js
+++ src/index.js
@@ -36,4 +36,4 @@ console.log(mixedInput("true"));
 type Suit = "Diamonds" | "Clubs" | "Spades";
 const diamonds: Suit = "Diamonds";
 const clubs: Suit = "Clubs";
-// const hearts: Suit = "Hearts"; This will generate error 
+// const hearts: Suit = "Hearts"; This will generate error
