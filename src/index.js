b854d03fca50e01ffe2fe01972e69057e4a18213
Arsalan Sadri <asadri62@gmail.com>
2018-12-29 14:02:17 -0800

    more practice

--- src/index.js
+++ src/index.js
@@ -2,3 +2,28 @@
 
 // VARIABLES
 let name: string = "John Doe";
+let age: number = 23;
+let users: string[] = ["David", "John", "Alan"];
+let ids: number[] = [34, 32, 31];
+let hasCourses: boolean = true;
+let someting: any = [1, 2, "three"];
+
+// INTERFACE
+interface Person {
+    name: string;
+    age: number;
+    hasKids?: boolean;
+}
+
+let person: Person;
+person = {
+    name: "Kate",
+    age: 45
+};
+
+// FUNCTION
+function greeting(name: string): string {
+    return `Hello ${name}!`;
+}
+
+console.log(greeting("Arsalan"));
